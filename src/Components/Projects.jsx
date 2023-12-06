8405ee7fe7e49da4e68295d506d19f9c5b667083
Jasmeet Singh <jasmeet5108k@gmail.com>
2023-12-06 17:27:54 +0530

    Updated Projects Section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
             <h2 className='font-bold text-3xl text-blue-600 mt-28 px-6 max-w-screen-xl mx-auto'>Projects</h2>
             <section id='projects' className='px-6 md:mt-8 max-w-screen-xl mx-auto md:flex md:flex-col md:items-center'>
 
-                <div id='Calculator' className='border lg:w-[850px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 mt-4 rounded-md'>
+                <div id='Calculator' className='border lg:w-[900px] lg:h-[300px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 mt-4 rounded-md'>
                     <img src="Images/calculator-Img.jpg" alt="" className='rounded-md w-full md:w-[350px]' />
                     <div className='md:flex md:flex-col md:space-y-4 md:justify-center'>
                         <p className='mt-4 text-2xl text-center md:text-left font-semibold text-blue-600'>Simple Calculator</p>
@@ -18,7 +18,7 @@ const Projects = () => {
                     </div>
                 </div>
 
-                <div id="Hosting" className='mt-12 border lg:w-[850px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 rounded-md'>
+                <div id="Hosting" className='mt-12 border lg:w-[900px] lg:h-[300px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 rounded-md'>
                     <img src="Images/Web-Hosting-Img.jpg" alt="" className='rounded-md w-full border md:w-[350px]' />
                     <div className='md:flex md:flex-col md:space-y-4 md:justify-center'>
                         <p className='mt-4 text-2xl text-center md:text-left font-semibold text-blue-600'>Web Hosting</p>
